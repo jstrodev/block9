8414009e5ecb42d9f0112beadd56ecfe49ea0492
jstro17 <jc@venalyze.com>
2024-09-12 15:37:39 -0500

    Added javascript file and alert message when a page loads.

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// Display an alert with the message "Hello, World!" once the page has loaded
+
+const alertMessage = () => {
+  alert("Hello, World!");
+}
+
+// Call function when the page has loaded
+window.onload = () => {
+  alertMessage();
+}
